130e52252957ca37455ecd579f7fa6639c644fb3
yogawicak <rwnhermawan@gmail.com>
2021-07-05 22:36:57 +0700

    theory and learning module fragment

---
+++ 1.Fundamental/MyFlexibleFragment/app/src/main/java/com/onedev/dicoding/myflexiblefragment/activity/ProfileActivity.kt
@@ -0,0 +1,12 @@
+package com.onedev.dicoding.myflexiblefragment.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.onedev.dicoding.myflexiblefragment.R
+
+class ProfileActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_profile)
+    }
+}
---
+++ 1.Fundamental/MyFlexibleFragment/app/src/main/java/com/onedev/dicoding/myflexiblefragment/fragment/DetailCategoryFragment.kt
@@ -0,0 +1,78 @@
+package com.onedev.dicoding.myflexiblefragment.fragment
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+import com.onedev.dicoding.myflexiblefragment.R
+import com.onedev.dicoding.myflexiblefragment.activity.ProfileActivity
+
+class DetailCategoryFragment : Fragment() {
+
+    var description: String? = null
+
+    lateinit var tvCategoryName: TextView
+    lateinit var tvCategoryDescription: TextView
+    lateinit var btnProfile: Button
+    lateinit var btnShowDialog: Button
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_detail_category, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        tvCategoryName = view.findViewById(R.id.tv_category_name)
+        tvCategoryDescription = view.findViewById(R.id.tv_category_description)
+        btnProfile = view.findViewById(R.id.btn_profile)
+        btnShowDialog = view.findViewById(R.id.btn_show_dialog)
+
+        btnProfile.setOnClickListener {
+            val mIntent = Intent(activity, ProfileActivity::class.java)
+            startActivity(mIntent)
+        }
+
+        btnShowDialog.setOnClickListener {
+            val mOptionDialogFragment = OptionDialogFragment()
+            val mFragmentManager = childFragmentManager
+            mOptionDialogFragment.show(mFragmentManager, OptionDialogFragment::class.java.simpleName)
+        }
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+        if (savedInstanceState != null) {
+            val descFromBundle = savedInstanceState.getString(EXTRA_DESCRIPTION)
+            description = descFromBundle
+        }
+
+        if (arguments != null) {
+            val categoryName = arguments?.getString(EXTRA_NAME)
+            tvCategoryName.text = categoryName
+            tvCategoryDescription.text = description
+        }
+    }
+
+    internal var optionDialogListener: OptionDialogFragment.OnOptionDialogListener = object :
+        OptionDialogFragment.OnOptionDialogListener {
+        override fun onOptionChosen(text: String?) {
+            Toast.makeText(activity, text, Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    companion object {
+        const val EXTRA_NAME = "extra_name"
+        const val EXTRA_DESCRIPTION = "extra_description"
+    }
+}
