e0966a82ccdc6a57c8fa2a1874316f4d6e47b007
yogawicak <rwnhermawan@gmail.com>
2021-08-10 20:57:00 +0700

    module 2 material design, action bar and, search view

---
+++ 2.Pengenalan_UI_UX/MyActionBar/app/src/main/java/com/onedev/dicoding/myactionbar/MainActivity.kt
@@ -0,0 +1,63 @@
+package com.onedev.dicoding.myactionbar
+
+import android.app.SearchManager
+import android.content.Context
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
+import androidx.appcompat.widget.SearchView
+import com.onedev.dicoding.myactionbar.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.option_menu, menu)
+
+        val searchManager = getSystemService(Context.SEARCH_SERVICE) as SearchManager
+        val searchView = menu.findItem(R.id.search).actionView as SearchView
+
+        searchView.setSearchableInfo(searchManager.getSearchableInfo(componentName))
+        searchView.queryHint = resources.getString(R.string.search_hint)
+        searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String): Boolean {
+                Toast.makeText(this@MainActivity, query, Toast.LENGTH_SHORT).show()
+                return true
+            }
+
+            override fun onQueryTextChange(newText: String): Boolean {
+                return false
+            }
+        })
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.menu1 -> {
+                supportFragmentManager.beginTransaction()
+                    .replace(R.id.fragment_container, MenuFragment())
+                    .addToBackStack(null)
+                    .commit()
+                true
+            }
+            R.id.menu2 -> {
+                startActivity(Intent(this, MenuActivity::class.java))
+                true
+            }
+            else -> {
+                true
+            }
+        }
+    }
+}
