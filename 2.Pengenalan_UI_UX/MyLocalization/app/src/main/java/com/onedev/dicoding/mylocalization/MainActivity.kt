a2d32cdef763bdf21db886dcc14fbdb3d2f282ef
yogawicak <rwnhermawan@gmail.com>
2021-08-12 09:54:42 +0700

    module 2 localization

---
+++ 2.Pengenalan_UI_UX/MyLocalization/app/src/main/java/com/onedev/dicoding/mylocalization/MainActivity.kt
@@ -0,0 +1,43 @@
+package com.onedev.dicoding.mylocalization
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.provider.Settings
+import android.view.Menu
+import android.view.MenuItem
+import com.onedev.dicoding.mylocalization.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val pokeCount = 3
+        val hello = resources.getString(R.string.hello_world, "Narenda Wicaksono", pokeCount, "Yoza Aprilio")
+        binding.tvHello.text = hello
+
+        val songCount = 5
+        val pluralText = resources.getQuantityString(R.plurals.numberOfSongsAvailable, songCount, songCount)
+        binding.tvPlural.text = pluralText
+
+        binding.tvXliff.text = resources.getString(R.string.app_homeurl)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main_menu, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == R.id.action_change_settings) {
+            val intent = Intent(Settings.ACTION_LOCALE_SETTINGS)
+            startActivity(intent)
+        }
+        return super.onOptionsItemSelected(item)
+    }
+}
