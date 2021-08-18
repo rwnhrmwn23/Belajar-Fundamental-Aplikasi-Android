b9bf4b61188537eb03af9fd38177ccf79bd898a7
yogawicak <rwnhermawan@gmail.com>
2021-08-18 12:52:55 +0700

    refactor submission two clean code

--- 3.Background_Process/Submission2/app/src/main/java/com/onedev/dicoding/submission_two/ui/HomeFragment.kt
+++ 3.Background_Process/Submission2/app/src/main/java/com/onedev/dicoding/submission_two/ui/HomeFragment.kt
@@ -4,7 +4,6 @@ import android.app.SearchManager
 import android.content.Context
 import android.os.Bundle
 import android.view.*
-import android.widget.Toast
 import androidx.appcompat.widget.SearchView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
