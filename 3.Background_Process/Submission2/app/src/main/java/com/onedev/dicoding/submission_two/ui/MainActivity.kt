69c900e8050796d2017dd4cd562d735bd33fe9db
yogawicak <rwnhermawan@gmail.com>
2021-08-19 10:56:18 +0700

    update submission two onBackpressed in activity

--- 3.Background_Process/Submission2/app/src/main/java/com/onedev/dicoding/submission_two/ui/MainActivity.kt
+++ 3.Background_Process/Submission2/app/src/main/java/com/onedev/dicoding/submission_two/ui/MainActivity.kt
@@ -2,6 +2,7 @@ package com.onedev.dicoding.submission_two.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.onedev.dicoding.submission_two.R
 import com.onedev.dicoding.submission_two.databinding.ActivityMainBinding
 import com.onedev.dicoding.submission_two.util.LocaleHelper.getPersistedLanguage
 import com.onedev.dicoding.submission_two.util.LocaleHelper.setLocale
@@ -17,5 +18,14 @@ class MainActivity : AppCompatActivity() {
 
         val language = getPersistedLanguage(this@MainActivity, "in")
         setLocale(this@MainActivity, language)
+    }
+
+    override fun onBackPressed() {
+        supportFragmentManager.apply {
+            if ((findFragmentById(R.id.main_navigation)?.childFragmentManager?.backStackEntryCount) ?: 0 > 1)
+                super.onBackPressed()
+            else
+                finish()
+        }
     }
 }
