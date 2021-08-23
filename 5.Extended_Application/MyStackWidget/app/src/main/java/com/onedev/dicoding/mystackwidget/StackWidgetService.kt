7eeb5a1af0a225781da7a9946c2f0ef8324ebc72
yogawicak <rwnhermawan@gmail.com>
2021-08-23 15:15:54 +0700

    update folder submission, module 5 widget and stack widget

---
+++ 5.Extended_Application/MyStackWidget/app/src/main/java/com/onedev/dicoding/mystackwidget/StackWidgetService.kt
@@ -0,0 +1,10 @@
+package com.onedev.dicoding.mystackwidget
+
+import android.content.Intent
+import android.widget.RemoteViewsService
+
+class StackWidgetService: RemoteViewsService() {
+    override fun onGetViewFactory(intent: Intent?): RemoteViewsFactory =
+        StackRemoteViewsFactory(this.applicationContext)
+
+}
